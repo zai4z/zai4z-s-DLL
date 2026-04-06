8a2d7a3d01385e247fc4c3640360c2315ae4c375
zai4z <piersnichols@gmail.com>
2026-04-06 18:08:23 +0100

    Added new city bombard art for each era

--- (1) Community Patch/Database Changes/Tech/CoreEraChanges.sql
+++ (1) Community Patch/Database Changes/Tech/CoreEraChanges.sql
@@ -1,19 +1,11 @@
 -- Modifier on warmonger score
-UPDATE Eras SET WarmongerPercent = 50 WHERE Type = 'ERA_ANCIENT';
-UPDATE Eras SET WarmongerPercent = 60 WHERE Type = 'ERA_CLASSICAL';
-UPDATE Eras SET WarmongerPercent = 70 WHERE Type = 'ERA_MEDIEVAL';
-UPDATE Eras SET WarmongerPercent = 80 WHERE Type = 'ERA_RENAISSANCE';
-UPDATE Eras SET WarmongerPercent = 90 WHERE Type = 'ERA_INDUSTRIAL';
-UPDATE Eras SET WarmongerPercent = 100 WHERE Type = 'ERA_MODERN';
-UPDATE Eras SET WarmongerPercent = 110 WHERE Type = 'ERA_POSTMODERN';
-UPDATE Eras SET WarmongerPercent = 120 WHERE Type = 'ERA_FUTURE';
-
 -- Update the base eras with the correct values
--- Yes, Medieval and Renaissance are correct values, blame the typo to Firaxis
-UPDATE Eras SET EraSplashImage = 'ERA_Classical.dds' WHERE Type = 'ERA_CLASSICAL';
-UPDATE Eras SET EraSplashImage = 'ERA_Medievel.dds' WHERE Type = 'ERA_MEDIEVAL';
-UPDATE Eras SET EraSplashImage = 'ERA_Renissance.dds' WHERE Type = 'ERA_RENAISSANCE';
-UPDATE Eras SET EraSplashImage = 'ERA_Industrial.dds' WHERE Type = 'ERA_INDUSTRIAL';
-UPDATE Eras SET EraSplashImage = 'ERA_Modern.dds' WHERE Type = 'ERA_MODERN';
-UPDATE Eras SET EraSplashImage = 'ERA_Atomic.dds' WHERE Type = 'ERA_POSTMODERN';
-UPDATE Eras SET EraSplashImage = 'ERA_Future.dds' WHERE Type = 'ERA_FUTURE';
+-- Also add new city bombard art for each era
+UPDATE Eras SET WarmongerPercent = 50, CityBombardEffectTag = 'ART_DEF_VEFFECT_AFRICAN_FOREST_SPEAR_STATIC' WHERE Type = 'ERA_ANCIENT';
+UPDATE Eras SET EraSplashImage = 'ERA_Classical.dds', WarmongerPercent = 60 WHERE Type = 'ERA_CLASSICAL';
+UPDATE Eras SET EraSplashImage = 'ERA_Medievel.dds', WarmongerPercent = 70  WHERE Type = 'ERA_MEDIEVAL';
+UPDATE Eras SET EraSplashImage = 'ERA_Renissance.dds', WarmongerPercent = 80, CityBombardEffectTag = 'ART_DEF_VEFFECT_HWACHA_ARROW' WHERE Type = 'ERA_RENAISSANCE';
+UPDATE Eras SET EraSplashImage = 'ERA_Industrial.dds', WarmongerPercent = 90, CityBombardEffectTag = 'ART_DEF_VEFFECT_WW1_BOMBER_PROJECTILE' WHERE Type = 'ERA_INDUSTRIAL';
+UPDATE Eras SET EraSplashImage = 'ERA_Modern.dds', WarmongerPercent = 100, CityBombardEffectTag = 'ART_DEF_VEFFECT_WW1_BOMBER_PROJECTILE' WHERE Type = 'ERA_MODERN';
+UPDATE Eras SET EraSplashImage = 'ERA_Atomic.dds', WarmongerPercent = 110 WHERE Type = 'ERA_POSTMODERN';
+UPDATE Eras SET EraSplashImage = 'ERA_Future.dds', WarmongerPercent = 120, CityBombardEffectTag = 'ART_DEF_VEFFECT_TRAIL_RAILGUN_PROJ' WHERE Type = 'ERA_FUTURE';
